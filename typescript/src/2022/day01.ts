8a7b70d393fbb7a3ee3d9c090c4abb05c7e7a940
Jzz <29693812+masiafrest@users.noreply.github.com>
2022-12-02 13:15:44 -0500

    refactor

--- typescript/src/2022/day01.ts
+++ typescript/src/2022/day01.ts
@@ -1,32 +1,24 @@
 import { getInputChallenge } from "../services/adventofcode";
 
-function run() {
-  partOne();
-}
+getInputChallenge({ year: 2022, day: 1 })
+  .then((res) => {
+    const data = res.data.split("\n");
+    const result: number[] = [];
+    let sum = 0;
 
-const partOne = () => {
-  getInputChallenge({ year: 2022, day: 1 })
-    .then((res) => {
-      const data = res.data.split("\n");
-      const result : number[]= []
-      let sum = 0
+    data.forEach((e: string) => {
+      if (e === "") {
+        result.push(sum);
+        sum = 0;
+        return;
+      }
+      sum += Number(e);
+    });
 
-      data.forEach( (e: string) => {
-        if(e === ''){
-          result.push(sum) 
-          sum = 0
-          return;
-        } 
-        sum += Number(e)
-      });
+    result.sort((a, b) => Number(b) - Number(a));
+    const first = result[0];
+    const total = first + result[1] + result[2];
+    console.log({ first, total });
+  })
+  .catch((err) => console.log({ err }));
 
-      result.sort((a, b) => Number(b) - Number(a))
-      const first = result[0]
-      const total = first + result[1] + result[2] 
-      console.log({first, total})
-
-    })
-    .catch((err) => console.log({ err }));
-};
-
-run();
